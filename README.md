17a572e946cd1ade7289494749c94dc7cd4bab8e
mehdi <powergame_coder2@yahoo.com>
2020-01-19 07:58:39 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,8 @@
+# TELEGRAM INFO BOT
+
+## An easy-to-use Telegram bot to fetch peer information
+
+
+
+
+### Powered by madelineProto
--- src/BotEventHandler.php
+++ src/BotEventHandler.php
@@ -18,5 +18,27 @@ class BotEventHandler extends EventHandler {
       }
 
       $this->logger($update);
+      $msgParts = explode(" ", $update['message']['message']);
+
+      switch($msgParts[0]) {
+        case '/id':
+          yield $this->messages->sendMessage(['peer' => $update['message']['from_id'], 'message' => "Your id is: {$update['message']['from_id']}"  ]);
+        break;
+
+        case '/channelid':
+          if(!isset($msgParts[1])) {
+            yield $this->messages->sendMessage(['peer' => $update['message']['from_id'], 'message' => "You must specify channel @id or link as in ```/channelid https://t.me/channel_name_goes_here```"]);
+              return;
+          }
+          try{
+            $details = yield $this->getPwrChat($msgParts[1]);
+            yield $this->messages->sendMessage(['peer' => $update['message']['from_id'], 'message' => "Type: {$details['type']}\n ID: {$details['id']}"]);
+          } catch(\Exception $e) {
+            yield $this->messages->sendMessage(['peer' => $update['message']['from_id'], 'message' => "Oh no! An error occurred."]);
+            $this->logger($e);
+          }
+        break;
+
+      }
     }
 }
